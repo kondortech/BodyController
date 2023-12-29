fa77eb8b946a28088689422006f6a4124ace4929
kirushka <kkondratiu@jacobs-university.de>
2023-12-29 22:09:06 +0100

    fixed recipe proto definition

--- domains/nutrition/models/recipe.mongo.go
+++ domains/nutrition/models/recipe.mongo.go
@@ -8,32 +8,32 @@ import (
 )
 
 type RecipeMongoDB struct {
-	Id                      primitive.ObjectID          `bson:"_id,omitempty"`
-	Title                   string                      `bson:"title"`
-	TasteDescription        string                      `bson:"taste_description"`
-	CookingStepsDescription string                      `bson:"cooking_steps_description"`
-	Author                  string                      `bson:"author"`
-	RequiredIngredients     []WeightedIngredientMongoDB `bson:"required_ingredients"`
-	CookingTime             int64                       `bson:"cooking_time"`
-	Macros100G              MacrosMongoDB               `bson:"macros_100g"`
+	Id                             primitive.ObjectID          `bson:"_id,omitempty"`
+	Title                          string                      `bson:"title"`
+	TasteDescription               string                      `bson:"taste_description"`
+	CookingStepsDescription        string                      `bson:"cooking_steps_description"`
+	Author                         string                      `bson:"author"`
+	RequiredIngredientsProportions []WeightedIngredientMongoDB `bson:"required_ingredients_proportions"`
+	CookingTime                    int64                       `bson:"cooking_time"`
+	Macros100G                     MacrosMongoDB               `bson:"macros_100g"`
 }
 
 func (proto *Recipe) ConvertToMongoDocument() (*RecipeMongoDB, error) {
 	mongoMacros, _ := proto.Macros100G.ConvertToMongoDocument()
 
 	mongo := &RecipeMongoDB{
-		Title:                   proto.Title,
-		TasteDescription:        proto.TasteDescription,
-		CookingStepsDescription: proto.CookingStepsDescription,
-		Author:                  proto.Author,
-		RequiredIngredients:     make([]WeightedIngredientMongoDB, 0, len(proto.RequiredIngredients)),
-		CookingTime:             proto.CookingTime.Seconds,
-		Macros100G:              *mongoMacros,
+		Title:                          proto.Title,
+		TasteDescription:               proto.TasteDescription,
+		CookingStepsDescription:        proto.CookingStepsDescription,
+		Author:                         proto.Author,
+		RequiredIngredientsProportions: make([]WeightedIngredientMongoDB, 0, len(proto.RequiredIngredientsProportions)),
+		CookingTime:                    proto.CookingTime.Seconds,
+		Macros100G:                     *mongoMacros,
 	}
 
-	for _, protoWeightedIngredient := range proto.RequiredIngredients {
+	for _, protoWeightedIngredient := range proto.RequiredIngredientsProportions {
 		mongoWeightedIngredient, _ := protoWeightedIngredient.ConvertToMongoDocument()
-		mongo.RequiredIngredients = append(mongo.RequiredIngredients, *mongoWeightedIngredient)
+		mongo.RequiredIngredientsProportions = append(mongo.RequiredIngredientsProportions, *mongoWeightedIngredient)
 	}
 
 	if len(proto.GetHexId()) != 0 {
@@ -50,19 +50,19 @@ func (proto *Recipe) ConvertToMongoDocument() (*RecipeMongoDB, error) {
 func (mongo *RecipeMongoDB) ConvertToProtoMessage() (*Recipe, error) {
 	protoMacros, _ := mongo.Macros100G.ConvertToProtoMessage()
 	proto := &Recipe{
-		HexId:                   mongo.Id.Hex(),
-		Title:                   mongo.Title,
-		TasteDescription:        mongo.TasteDescription,
-		CookingStepsDescription: mongo.CookingStepsDescription,
-		Author:                  mongo.Author,
-		RequiredIngredients:     make([]*WeightedIngredient, 0, len(mongo.RequiredIngredients)),
-		CookingTime:             &durationpb.Duration{Seconds: mongo.CookingTime},
-		Macros100G:              protoMacros,
+		HexId:                          mongo.Id.Hex(),
+		Title:                          mongo.Title,
+		TasteDescription:               mongo.TasteDescription,
+		CookingStepsDescription:        mongo.CookingStepsDescription,
+		Author:                         mongo.Author,
+		RequiredIngredientsProportions: make([]*WeightedIngredient, 0, len(mongo.RequiredIngredientsProportions)),
+		CookingTime:                    &durationpb.Duration{Seconds: mongo.CookingTime},
+		Macros100G:                     protoMacros,
 	}
 
-	for _, mongoWeightedIngredient := range mongo.RequiredIngredients {
+	for _, mongoWeightedIngredient := range mongo.RequiredIngredientsProportions {
 		protoWeightedIngredient, _ := mongoWeightedIngredient.ConvertToProtoMessage()
-		proto.RequiredIngredients = append(proto.RequiredIngredients, protoWeightedIngredient)
+		proto.RequiredIngredientsProportions = append(proto.RequiredIngredientsProportions, protoWeightedIngredient)
 	}
 
 	return proto, nil
