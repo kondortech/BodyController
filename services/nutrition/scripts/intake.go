8040c30743d9c0501b690c813783ab2eff056909
kirushka <kkondratiu@jacobs-university.de>
2024-04-28 13:26:12 +0200

    added script to test meal

--- services/nutrition/scripts/intake.go
+++ services/nutrition/scripts/intake.go
@@ -7,6 +7,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	pbModels "github.com/kirvader/BodyController/services/nutrition/models"
 	pb "github.com/kirvader/BodyController/services/nutrition/proto"
@@ -35,11 +37,41 @@ func main() {
 	// 	},
 	// })
 
-	resp, err := client.CreateRecipe(context.Background(), &pb.CreateRecipeRequest{
-		Entity: &pbModels.Recipe{
-			Name:        "Normal recipe 2",
-			RecipeSteps: "just do it",
-			ExampleIngredientsProportions: []*pbModels.WeightedIngredient{
+	// resp, err := client.CreateRecipe(context.Background(), &pb.CreateRecipeRequest{
+	// 	Entity: &pbModels.Recipe{
+	// 		Name:        "Normal recipe 2",
+	// 		RecipeSteps: "just do it",
+	// 		ExampleIngredientsProportions: []*pbModels.WeightedIngredient{
+	// 			{
+	// 				Ingredient: &pbModels.Ingredient{
+	// 					Id:   primitive.NewObjectID().Hex(),
+	// 					Name: "Some good shit 2",
+	// 					MacrosForWeight: &pbModels.MacrosForWeight{
+	// 						Macros: &pbModels.Macros{
+	// 							Calories: 10,
+	// 							Proteins: 0,
+	// 							Carbs:    2,
+	// 							Fats:     0,
+	// 						},
+	// 						Gramms: 100,
+	// 					},
+	// 				},
+	// 				Gramms: 1000,
+	// 			},
+	// 		},
+	// 	},
+	// })
+	// _, err = client.DeleteRecipe(context.Background(), &pb.DeleteRecipeRequest{
+	// 	EntityId: "662d4f93bd17f749e0aea14e",
+	// })
+
+	resp, err := client.CreateMeal(context.Background(), &pb.CreateMealRequest{
+		Entity: &pbModels.Meal{
+			Username: "Kirill",
+			RecipeId: &wrapperspb.StringValue{
+				Value: "662d4fffbd17f749e0aea14f",
+			},
+			WeightedIngredients: []*pbModels.WeightedIngredient{
 				{
 					Ingredient: &pbModels.Ingredient{
 						Id:   primitive.NewObjectID().Hex(),
@@ -57,10 +89,9 @@ func main() {
 					Gramms: 1000,
 				},
 			},
+			Timestamp:  timestamppb.Now(),
+			MealStatus: pbModels.MealStatus_Consumed,
 		},
-	})
-	_, err = client.DeleteRecipe(context.Background(), &pb.DeleteRecipeRequest{
-		EntityId: "662d4f93bd17f749e0aea14e",
 	})
 	if err != nil {
 		log.Printf("error: %v", err)
